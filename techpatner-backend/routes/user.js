698f3930779d72be021e03d35968b1e39fcfee7d
Yuvraj Raghuvanshi <124128352+yuvrajraghuvanshi@users.noreply.github.com>
2025-07-14 21:43:31 +0530

    added stats api

--- techpatner-backend/routes/user.js
+++ techpatner-backend/routes/user.js
@@ -1,9 +1,17 @@
-const { listUser, updateUser } = require("../controllers/user.controller");
+const { listUser, updateUser, getUserStats, getUserDistribution, getPlanDistribution, getRevenueTrends, getRecentUsers } = require("../controllers/user.controller");
 const handleValidationErrors = require("../utils/validateErrors");
 const updateUserValidation = require("../validators/user.validator");
 
 const router=require("express").Router()
 
+//stats
+router.get("/stats",handleValidationErrors,getUserStats)
+router.get("/user-distribution",handleValidationErrors,getUserDistribution)
+router.get("/plan-distribution",handleValidationErrors,getPlanDistribution)
+router.get("/revenue-trends",handleValidationErrors,getRevenueTrends)
+router.get("/recent-users",handleValidationErrors,getRecentUsers)
+
+//users
 router.get("/",handleValidationErrors,listUser)
 router.put("/:id/update",handleValidationErrors,updateUserValidation,updateUser)
 
